491f0a93c03c481dea686914f004bc083af3b880
Paras Chauhan <paraspratapsingh067@gmail.com>
2025-10-07 20:51:31 +0530

    Updated RestHeader.js UI and component changes

--- src/Components/RestHeader.js
+++ src/Components/RestHeader.js
@@ -7,7 +7,7 @@ export default function RestHeader(){
     const counter = useSelector(state=> state.cartslice.count);
     
     return (
-        <div className="container w-[80%] mx-auto py-4 px-8 bg-gray-200 text-5xl flex justify-between items-center">
+        <div className="container w-[80%] mx-auto mt-2 py-4 px-8 bg-gray-200 text-5xl flex justify-between items-center rounded-xl">
             <div>
                 <p className="text-orange-600 font-bold">Swiggy</p>
             </div>
